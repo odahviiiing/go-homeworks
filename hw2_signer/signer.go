67cf963035bec5d8acb0f81e41cc2ce37d9ee0e9
pavel.ivankov <pavel.ivankov@tages.ru>
2021-02-08 10:30:35 +0300

    signer

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -6,7 +6,6 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"sync/atomic"
 )
 
@@ -22,12 +21,14 @@ type MultiHashIndexedResult struct {
 var inputDataCount uint32 = 0
 var combineResultsCount uint32 = 0
 var combineResultsSlice []string
-var mutex = &sync.Mutex{}
+var cancelChannel = make(chan interface{})
 
 func SingleHash(in, out chan interface{}) {
+
+	fmt.Println("in SingleHash")
 	for currVal := range in {
 		data := fmt.Sprintf("%v", currVal)
-		//atomic.AddUint32(&inputDataCount, 1)
+		atomic.AddUint32(&inputDataCount, 1)
 		//inputDataCount++
 
 		//crcChan := make(chan string)
@@ -61,6 +62,7 @@ func multiHashWorker(index int, data string, out chan<- MultiHashIndexedResult) 
 }
 
 func MultiHash(in, out chan interface{}) {
+	fmt.Println("in MultiHash")
 	for currVal := range in {
 		data := fmt.Sprintf("%v", currVal)
 		result := ""
@@ -88,32 +90,38 @@ func MultiHash(in, out chan interface{}) {
 }
 
 func CombineResults(in, out chan interface{}) {
+	fmt.Println("in CombineResults")
 	for currVal := range in {
 		data, ok := currVal.(string)
 		if !ok {
 			panic("cant convert result data to string")
 		}
 
-		mutex.Lock()
 		combineResultsSlice = append(combineResultsSlice, data)
-		mutex.Unlock()
 		atomic.AddUint32(&combineResultsCount, 1)
-		//loadedInputDataCount := atomic.LoadUint32(&inputDataCount)
-		//if uint32(len(combineResultsSlice)) < loadedInputDataCount {
-		//	continue
-		//}
-		if combineResultsCount == 3 {
-			sort.Strings(combineResultsSlice)
 
-			var result interface{} = strings.Join(combineResultsSlice, "_")
-			fmt.Printf("combine result: %s\n", strings.Join(combineResultsSlice, "_"))
-			out <- result
+		loadedInputDataCount := atomic.LoadUint32(&inputDataCount)
+		if uint32(len(combineResultsSlice)) < loadedInputDataCount {
+			runtime.Gosched()
+			continue
 		}
+		//if combineResultsCount == 3 {
+		sort.Strings(combineResultsSlice)
+
+		var result interface{} = strings.Join(combineResultsSlice, "_")
+		fmt.Printf("combine result: %s\n", strings.Join(combineResultsSlice, "_"))
+		out <- result
+		//close(out)
+		cancelChannel <- struct{}{}
+		//}
+
 		runtime.Gosched()
 	}
 }
 
 func ExecutePipeline(jobs ...job) {
+
+	//fmt.Println("in ExecutePipeline")
 	jobsLength := len(jobs)
 	channelsLength := jobsLength + 1
 
@@ -123,31 +131,48 @@ func ExecutePipeline(jobs ...job) {
 	}
 	//fmt.Println(len(channels))
 
+	//fmt.Println("in ExecutePipeline jobs length: ", jobsLength)
 	for i := 0; i < jobsLength; i++ {
+		//fmt.Println("in ExecutePipeline loop jobs")
 		//fmt.Printf("job[%d] in(i): %d\tout(i+1): %d\n", i, i, i+1)
 		go jobs[i](channels[i], channels[i+1])
 		runtime.Gosched()
 	}
+
+	//fmt.Println("in ExecutePipeline after loop")
+
+	//LOOP:
+	//	for {
+	//		select {
+	//		case <-cancelChannel:
+	//			break LOOP
+	//		default:
+	//			runtime.Gosched()
+	//			continue
+	//		}
+	//	}
+	//	fmt.Scanln()
 }
 
 //func main() {
-//	//jab := []job{
-//	//	job(func(in, out chan interface{}) {
-//	//		fmt.Println("first")
-//	//		inputData := []int{0, 1, 1, 2, 3, 5, 8}
-//	//		for _, v := range inputData {
-//	//			out <- v
-//	//		}
-//	//	}),
-//	//	job(SingleHash),
-//	//	job(MultiHash),
-//	//	job(CombineResults),
-//	//	job(func(in, out chan interface{}) {
-//	//		dataRaw := <-in
-//	//		fmt.Printf("data returned %v: ", dataRaw)
-//	//	}),
-//	//}
-//	//ExecutePipeline(jab...)
+//	jab := []job{
+//		job(func(in, out chan interface{}) {
+//			fmt.Println("first")
+//			inputData := []int{0, 1, 1, 2, 3, 5, 8}
+//			for _, v := range inputData {
+//				out <- v
+//			}
+//		}),
+//		job(SingleHash),
+//		job(MultiHash),
+//		job(CombineResults),
+//		job(func(in, out chan interface{}) {
+//			dataRaw := <-in
+//			fmt.Printf("data returned %v: ", dataRaw)
+//			cancelChannel <- struct{}{}
+//		}),
+//	}
+//	ExecutePipeline(jab...)
 //
 //	var ok = true
 //	var recieved uint32
@@ -155,13 +180,12 @@ func ExecutePipeline(jobs ...job) {
 //		job(func(in, out chan interface{}) {
 //
 //			fmt.Println("first")
-//			fmt.Println("first")
 //			out <- 1
 //			fmt.Println("first data send")
 //			time.Sleep(10 * time.Millisecond)
 //			fmt.Println("first after timeout")
 //			currRecieved := atomic.LoadUint32(&recieved)
-//			fmt.Printf("value")
+//			fmt.Printf("value: %d\n", currRecieved)
 //			// в чем тут суть
 //			// если вы накапливаете значения, то пока вся функция не отрабоатет - дальше они не пойдут
 //			// тут я проверяю, что счетчик увеличился в следующей функции
@@ -175,6 +199,7 @@ func ExecutePipeline(jobs ...job) {
 //			for _ = range in {
 //				fmt.Println("second data recieved")
 //				atomic.AddUint32(&recieved, 1)
+//				cancelChannel <- struct{}{}
 //			}
 //		}),
 //	}
@@ -183,5 +208,6 @@ func ExecutePipeline(jobs ...job) {
 //	if !ok || recieved == 0 {
 //		fmt.Printf("no value free flow - dont collect them")
 //	}
+//	fmt.Println("test ok")
 //	fmt.Scanln()
 //}
